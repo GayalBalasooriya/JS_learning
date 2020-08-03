d9c48b1e30ffe3090ed6092ea44a0194eca8850e
Gayal Balasooriya <baigayal01@gmail.com>
2020-08-03 23:27:01 +0530

    answer for algorithm

--- Sandbox/app.js
+++ Sandbox/app.js
@@ -1,3 +1,37 @@
 //alert('hello')
-let val = Math.min(8,3);
-console.log(val);
+// let val = Math.min(8,3);
+// console.log(val);
+ 
+// var id = 100;
+
+// if(id == 'undefined') {
+//     console.log("Id is undefined");
+// }
+// else {
+//     console.log("Id is defined");
+// }
+
+// (function(name) {
+//     console.log(`Hello i'm ${name}`);
+// })('Gayal')
+
+const arr = [ undefined, 'hi', null, 50, -1, -5, 2, 'yo'];
+
+// for(let i=0; i<arr.length; i++) {
+//     // if(typeof arr[i] !== 'number') {
+//     //     arr.splice(arr[i]);
+//     // }
+// }
+
+const arr2 = arr.filter((val) => {
+    return typeof val == 'number';
+});
+console.log(arr2);
+if(arr2.length == 0) {
+    console.log("No numbers in array");
+} else {
+    arr2.sort(function(x,y) {
+        return x-y;
+    });
+    console.log(arr2[0]);
+}
