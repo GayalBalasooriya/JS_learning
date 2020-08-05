01a45d0a6ce08441629ac5578e502fa27f235eb2
Gayal Balasooriya <baigayal01@gmail.com>
2020-08-05 23:19:42 +0530

    added local storage

--- BookList/app.js
+++ BookList/app.js
@@ -54,6 +54,51 @@ class Book {
       document.getElementById('isbn').value = '';
     }
   }
+
+  //Local Storage Class
+  class Store{
+
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }   
+    
+    static displayBooks() {
+        const books = Store.getBooks();
+        books.forEach(function(book) {
+            const ui = new UI();
+
+            //Add book to UI
+            ui.addBookToList(book);
+        })
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function(book, index) {
+            if(book.isbn === isbn) {
+                books.splice(index, 1)
+            }
+        })
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
+
+  //Dom load event
+  document.addEventListener('DOMContentLoaded', Store.displayBooks);
   
   // Event Listener for add book
   document.getElementById('book-form').addEventListener('submit', function(e){
@@ -77,6 +122,9 @@ class Book {
     } else {
       // Add book to list
       ui.addBookToList(book);
+
+      //Add to LS
+      Store.addBook(book);
   
       // Show success
       ui.showAlert('Book Added!', 'success');
@@ -96,6 +144,9 @@ class Book {
   
     // Delete book
     ui.deleteBook(e.target);
+
+    //Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
   
     // Show message
     ui.showAlert('Book Removed!', 'success');
