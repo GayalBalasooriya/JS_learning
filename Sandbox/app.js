f33461d9489a79077819eb62b3b90ad1bc3b83d1
Gayal Balasooriya <baigayal01@gmail.com>
2020-08-11 22:40:24 +0530

    practice

--- Sandbox/app.js
+++ Sandbox/app.js
@@ -97,8 +97,86 @@ http.put('http://jsonplaceholder.typicode.com/users/2', data)
 */
 
 
+/*
 // delete post
 http.delete('http://jsonplaceholder.typicode.com/users/2')
     .then(data => console.log(data))
     .catch(err => console.log(err));
+*/
+
+/*
+class Stack{
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        if(this.items.length == 0)
+            return "underflow"
+        return this.items.pop();
+    }
+
+    display() {
+        if(this.items.length == 0) {
+            return "no items in array"
+        }
+        return this.items.forEach((ele) => {
+            console.log(" "+ ele);
+        })
+    }
+}
+
+let stack = new Stack();
+stack.push(10);
+stack.push(30)
+stack.display();
+*/
+
+// var strArray = [ "q", "w", "w", "e", "i", "u", "r"];
+
+// console.log(strArray.sort());
+
+// for(i=0; i<strArray.length-1; i++) {
+//     if(strArray[i] == strArray[i+1]) {
+//         console.log("Duplicate " + strArray[i]);
+//         i += 1;
+//     }
+// }
+
+let arr = [5, "hi", 1, 46, 32, 12, "asfda", undefined, null, -2, -9];
+
+// let newArray = arr.filter((ele) => {
+//     return typeof ele == 'number'
+// });
+
+// console.log(newArray);
+
+// if(newArray.length == 0) {
+//     console.log("No numbers in array");
+// }
+// else {
+//     let newest = newArray.sort((x,y) => {
+//         return x-y; 
+//     })
+//     console.log(newArray);
+// }
+var a = arr[3].toString();
+//arr.join('');
+let c = 12345;
+let b = c.toString();
+console.log(b)
+console.log(typeof b)
+let arrr = b.split('') 
+console.log(arrr)
+arrr.reverse()
+console.log(arrr)
+let arrrr = arrr.join('')
+console.log(arrrr)
+
+
+
 
